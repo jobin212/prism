54166340a4a8d6bd22f50b713b3305413deec734
Joseph Tobin <jobin212@gmail.com>
2018-01-11 11:45:40 -0500

    connected with localhost

--- App.jsx
+++ App.jsx
@@ -1,1 +1,48 @@
-export default () => '🔥 Ready.'
+// db without {} using default
+// {} check the export 
+import React from 'react'
+import {db} from './fire'
+
+// export default () => '🔥 Ready.'
+export default class Colors extends React.Component {
+
+    // componentDidMount things we want to listening
+    componentWillMount(){
+        this.unsubscribe = 
+            db.collection('colors4')
+                .onSnapshot(snap => 
+                    this.setState({
+                        colors: snap.docs.map(doc => doc.data())
+                    }))
+    }
+
+    // stop listening
+    componentWillUnmount(){
+        this.unsubscribe()
+    }
+
+
+    render(){
+        if(!this.state){
+            return "loading..."
+        }
+        const {colors} = this.state
+        // show everything return <pre>{JSON.stringify(this.state,0,2)}</pre>
+        return <div style ={{
+            display:'flex',
+            flexFlow:'row wrap',
+        }}> {
+                colors.map(color => <div style={{
+                width:'250px',
+                height:'250px',
+                margin: '9px',
+                backgroundColor:rgb(color)
+            }}>
+            <pre>{JSON.stringify(color.names,0,2)}</pre>
+            hsl({color.hue}, {color.saturation}, {color.luminance})
+        </div>)
+    }</div>
+    }
+}
+
+const rgb = color => `rgb(${color.red},${color.blue},${color.green}`
--- fire/seed.js
+++ fire/seed.js
@@ -6,20 +6,23 @@ var cf = require('color-functions');
 const db = firebase.firestore()
 
 const colors = require('../data/colors')
+const small_colors = require('../data/small-colors')
+const ref = db.collection('colors4')
 
 // TODO: Seed the database with colors
 
 
 
-colors.map( (c) => {
+small_colors.map( (c) => {
 
     const {name, color: {r,g, b}} = c;
     var id = cf.rgb2hex(r, g , b);
+    const hsv = cf.rgb2hsv(r, g, b);
+    const {h, s, l} = cf.hsv2hsl(hsv.h, hsv.s, hsv.v);
 
-    db.collection('colors').doc(id).set({
-        red : r,
-        green: g,
-        blue: b,
+    ref.doc(id).set({
+        red : r, green: g, blue: b,
+        hue : h, saturation: s, luminance: l,
         names: {
             [name]: true
         },
