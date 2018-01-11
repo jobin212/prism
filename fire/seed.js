ac74761c7207ff103bdea1bbae86bc816323bae2
Joseph Tobin <jobin212@gmail.com>
2018-01-11 11:26:11 -0500

    sending data to firebase

--- fire/seed.js
+++ fire/seed.js
@@ -1,9 +1,33 @@
 const firebase = require('firebase')
 require('firebase/firestore')
 require('./setup')(firebase)
+var cf = require('color-functions');
 
 const db = firebase.firestore()
 
-const colors = require('~/data/colors')
+const colors = require('../data/colors')
 
 // TODO: Seed the database with colors
+
+
+
+colors.map( (c) => {
+
+    const {name, color: {r,g, b}} = c;
+    var id = cf.rgb2hex(r, g , b);
+
+    db.collection('colors').doc(id).set({
+        red : r,
+        green: g,
+        blue: b,
+        names: {
+            [name]: true
+        },
+    }, {merge: true})
+    .then(() => {
+        console.log(`wrote ${id} as ${name}`)
+    })
+    .catch(console.error);
+})
+
+
--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyB4v5bzeeWjQ5zawTLsRy_bmNZ_bScKgYo",
+  "databaseURL": "https://meese-herder.firebaseio.com",
+  "storageBucket": "meese-herder.appspot.com",
+  "authDomain": "meese-herder.firebaseapp.com",
+  "messagingSenderId": "875967521211",
+  "projectId": "meese-herder"
+});
+
